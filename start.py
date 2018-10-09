2649082249a8e77b438d6d13f46f88bcdda7885c
Derrick Adjei <adjeidr@vcu.edu>
2018-10-09 10:19:54 -0400

    began predictions portion

--- start.py
+++ start.py
@@ -5,10 +5,10 @@ import ast
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import sklearn.preprocessing as pre
+import sklearn.preprocessing as preprocessing
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.datasets import load_breast_cancer
-
+#percision, recall, f measure, confusion matrix
 
 def dataChanger(dataArr):
     hold = list()
@@ -17,25 +17,41 @@ def dataChanger(dataArr):
     return hold
 
 file = 'citalopram_train.csv'
+test = 'test.csv'
+tfile = 'citalopram_test.csv'
+out = 'randF.csv'
 df = pandas.read_csv(file, names = ["data","target"])
-print(df.shape)
+
 data = np.array(df["data"])
 target = np.array(df["target"])
 
 data = dataChanger(data)
 
-v = DictVectorizer(sparse=False)
-v.fit_transform(data)
+v = DictVectorizer()
+enc = preprocessing.LabelEncoder()
 
-enc = pre.LabelEncoder()
-print(enc.fit_transform(target))
+x = v.fit_transform(data)
+y = enc.fit_transform(target)
 
+print(x)
+print(y)
 
-# cancer = load_breast_cancer()
-# x_train, x_test, y_train, y_test = train_test_split(v, enc, random_state = 0)
+# x_train, x_test, y_train, y_test = train_test_split(x, y, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+forest = RandomForestClassifier(n_estimators=100, random_state=0)
+forest.fit(x_train, y_train)
+print("Accuracy of training the data set {:.3f}".format(forest.score(x_train, y_train)))
+print("Accuracy of of the test subset {:.3f}".format(forest.score(x_test,y_test)))
+#run against test set
+df2 = pandas.read_csv(tfile, names = ["data","target"])
+test_features = np.array(df2["data"])
+test_target = np.array(df2["target"])
+test_features = dataChanger(test_features)
+
+xt = v.fit_transform(test_features)
+yt = enc.fit_transform(target)
 #
-# forest = RandomForestClassifier(n_estimators=100, random_state=0)
-# forest.fit(x_train, y_train)
+# predictions = forest.predict(xt)
 #
-# print("Accuracy of training the data set {:.3f}".format(forest.score(x_train, y_train)))
-# print("Accuracy of of the test subset {:.3f}".format(forest.score(x_test,y_test)))
+# print(predictions)
